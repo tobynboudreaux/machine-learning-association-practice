12d93a323e853a439a4d2c86f24eee0e6f0e0ff5
tobynboudreaux <toby.boudreaux11@gmail.com>
2020-09-12 17:58:15 -0500

    Eclat

--- eclat.py
+++ eclat.py
@@ -9,7 +9,7 @@ transactions = []
 for i in range(0, 7501):
   transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
 
-# Training the Apriori model on the dataset
+# Training the Eclat model on the dataset
 from apyori import apriori
 rules = apriori(transactions = transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2, max_length = 2)
 
@@ -23,13 +23,8 @@ def inspect(results):
     lhs         = [tuple(result[2][0][0])[0] for result in results]
     rhs         = [tuple(result[2][0][1])[0] for result in results]
     supports    = [result[1] for result in results]
-    confidences = [result[2][0][2] for result in results]
-    lifts       = [result[2][0][3] for result in results]
-    return list(zip(lhs, rhs, supports, confidences, lifts))
-resultsinDataFrame = pd.DataFrame(inspect(results), columns = ['Left Hand Side', 'Right Hand Side', 'Support', 'Confidence', 'Lift'])
+    return list(zip(lhs, rhs, supports))
+resultsinDataFrame = pd.DataFrame(inspect(results), columns = ['Product 1', 'Product 2', 'Support'])
 
-## Displaying the results non sorted
-print(resultsinDataFrame)
-
-## Displaying the results sorted by descending lifts
-print(resultsinDataFrame.nlargest(n = 10, columns = 'Lift'))
+## Displaying the results sorted by descending supports
+print(resultsinDataFrame.nlargest(n = 10, columns = 'Support'))
